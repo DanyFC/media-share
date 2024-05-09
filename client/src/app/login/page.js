057c98dcd07da832f98d6c51304318cbe9a93b4b
DanyFC <danyfarfanx@gmail.com>
2024-05-09 12:00:31 -0500

    feat: Added the alerts and replaced the inputs with the component

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -1,8 +1,16 @@
 "use client"
+import { AuthContext } from '@/context/auth/context'
+import { useContext, useEffect } from 'react'
 import { useFormik } from 'formik'
+import { useRouter } from 'next/navigation'
 import * as Yup from 'yup'
+import Alert from '@/components/alert'
+import FormField from '@/components/formField'
 
 const Login = () => {
+  const { authenticated, error, logIn } = useContext(AuthContext)
+  const router = useRouter()
+
   const formik = useFormik({
     initialValues: {
       account: '',
@@ -15,41 +23,30 @@ const Login = () => {
         .min(8, 'Password should have at least 8 characters')
         .required('Please enter your password')
     }),
-    onSubmit: values => {
-      console.log(values)
+    onSubmit: async (values) => {
+      try {
+        await logIn(values)
+      } catch (err) {
+        console.log(err.message)
+      }
     }
   })
+
+  useEffect(()=>{
+    if(authenticated){
+      router.push('/')
+    }
+  }, [authenticated])
 
   return (
     <div className='md:w-4/5 xl:w-3/5 mx-auto mb-2'>
       <h2 className='text-4xl font-bold text-gray-800 text-center my-4'>Log In</h2>
+      {error && <Alert message={error} />}
       <div className='flex justify-center mt-5'>
         <div className='w-full max-w-lg'>
           <form className='bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4' onSubmit={formik.handleSubmit}>
-            <div className='mb-4'>
-              <label className='block text-black text-sm font-bold mb-2' htmlFor='account'>Account</label>
-              <input type='text' id='account' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-md' placeholder='your Username' value={formik.values.account} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-              {
-                formik.touched.account && formik.errors.account
-                  ? (<div className='my-2 bg-gray-200 border-l-4 border-red-500 text-primary p-4'>
-                    <p className='font-bold'>Error:</p>
-                    <p>{formik.errors.account}</p>
-                  </div>)
-                  : null
-              }
-            </div>
-            <div className='mb-4'>
-              <label className='block text-black text-sm font-bold mb-2' htmlFor='password'>Password</label>
-              <input type='password' id='password' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-md' placeholder='●●●●●●●●' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-              {
-                formik.touched.password && formik.errors.password
-                  ? (<div className='my-2 bg-gray-200 border-l-4 border-red-500 text-primary p-4'>
-                    <p className='font-bold'>Error:</p>
-                    <p>{formik.errors.password}</p>
-                  </div>)
-                  : null
-              }
-            </div>
+            <FormField formik={formik} id={'account'} label={'Account'} placeholder={'Your account'} type={'text'} />
+            <FormField formik={formik} id={'password'} label={'Password'} placeholder={'●●●●●●●●'} type={'password'} />
             <input type='submit' className='bg-primary hover:bg-black w-full p-2 text-white uppercase font-bold' value='Continue' />
           </form>
         </div>
