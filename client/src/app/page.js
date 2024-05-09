df6d7102bbf7891408937a96ad8d3d90f0826d5b
DanyFC <danyfarfanx@gmail.com>
2024-05-09 12:03:53 -0500

    feat: Session validation at home
    It is validated if the localStorage token is valid and if it is, it modifies the user's authentication status to true.

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,7 +1,23 @@
+"use client"
+import { AuthContext } from "@/context/auth/context"
+import { useContext, useEffect } from "react"
 
 const Home = () => {
+  const { authenticated, validateSession } = useContext(AuthContext)
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await validateSession()
+      } catch (error) {
+        console.log(error.message)
+      }
+    })()
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>Home
+    </div>
   )
 }
 
