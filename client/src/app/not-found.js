47065ff70155c86fa0b787f6864100ec1cd557f9
DanyFC <danyfarfanx@gmail.com>
2024-05-13 18:33:44 -0500

    feat: Added 404 page

---
+++ client/src/app/not-found.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className='text-center py-10 text-5xl'>
+        <span className='text-primary font-bold'>404</span>&nbsp;| This page could not be found
+      </h1>
+    </div>
+  )
+}
+
+export default NotFound
